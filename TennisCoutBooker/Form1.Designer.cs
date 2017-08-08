774a1423ebcb4632440ee62a149d2a42c7a40f14
vtw唐巍 <weitang@ctrip.com>
2017-08-08 19:46:12 +0800

    init

---
+++ TennisCoutBooker/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TennisCoutBooker
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bookingWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bookingWebBrowser
+            // 
+            this.bookingWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bookingWebBrowser.Location = new System.Drawing.Point(0, 0);
+            this.bookingWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.bookingWebBrowser.Name = "bookingWebBrowser";
+            this.bookingWebBrowser.Size = new System.Drawing.Size(848, 588);
+            this.bookingWebBrowser.TabIndex = 0;
+            this.bookingWebBrowser.Url = new System.Uri("http://sjtuedu.iydsj.com/login/outerLogin", System.UriKind.Absolute);
+            this.bookingWebBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.bookingWebBrowser_DocumentCompleted);
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(411, 12);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(100, 21);
+            this.userNameTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(724, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(563, 12);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 21);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 588);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.bookingWebBrowser);
+            this.Name = "mainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser bookingWebBrowser;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox passwordTextBox;
+    }
+}
+
---
+++ TennisCoutBooker/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace TennisCoutBooker
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bookingWebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var inputElements = this.bookingWebBrowser.Document.DomDocument;
+        }
+    }
+}
